132e0862736e1e8d520a2a4f15638d00af753799
Gaetano Sabino <gaetano.sabino@gmail.com>
2018-10-30 11:57:16 -0500

    Creamos el CRud

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,21 @@
 // ==================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ==================
+// Entorno
+// ==================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==================
+// Base de datos
+// ==================
+
+let urlBD;
+if (process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe';
+} else {
+    urlBD = 'mongodb://unico-prova:Gaetano1979@ds145573.mlab.com:45573/unico';
+}
+process.env.URLDB = urlBD;
--- server/server.js
+++ server/server.js
@@ -2,63 +2,38 @@ require('./config/config');
 
 const express = require('express');
 const app = express();
+
+//creamos el vinculo con la rutas de usuarios
+app.use(require('./routers/usuarios'));
+
+//creamos conection con mongoose
+const mongoose = require('mongoose');
+
 //declaramos el porto como costante 
 const port = process.env.PORT || 3000;
+
 //declaramos el parser 
 const bodyParser = require('body-parser');
 
-//configuaraciones 
-
-
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
-
-
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     //res.send('Buenos dias a todos');
     //creamos un archivo json
-    res.json('Buenos dias a todos');
+    res.json('Buenos dias a todos en Local');
 });
 
-app.get('/usuarios', (req, res) => {
-    // let cuerpopeticion=req.body;
-    res.json('Listas Usuarios');
-});
-
-app.post('/usuarios', (req, res) => {
-    let peticion = req.body;
-    if (peticion.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario',
-        });
-    } else {
-        //cambio respuesta
-        // res.json('Pagina Post usuarios');
-        res.json({
-            body: peticion
-        });
-
-    }
 
 
-});
-app.put('/usuarios', (req, res) => {
-    res.json('Pagina Put Usuarios');
-});
-app.put('/usuarios/:id', (req, res) => {
-    let idUsuario = req.params.id;
-    res.json({
-        id: idUsuario,
+//creamos conection
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw new Error('Conession no establesida ' + err);
+    console.log('Base de datos ONLINE');
 
-    });
-})
-app.delete('/usuarios', (req, res) => {
-    res.json('Pagina Delete Usuarios');
 });
 
 app.listen(process.env.PORT, () => {
